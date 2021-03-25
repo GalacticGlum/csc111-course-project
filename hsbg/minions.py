1ac6d4d95e2140606856c80d58c5f165ad2fb740
GalacticGlum <nerftactic09@gmail.com>
2021-03-25 18:43:26 -0400

    Added implementation of tier 1 minions

--- hsbg/minions.py
+++ hsbg/minions.py
@@ -1,21 +1,317 @@
-"""Implementation of various minion cards."""
-from hsbg.models import Minion, MinionAbility, MinionKeyword, MinionType, TIER_RANKS
+"""Implementation of various minion cards.
 
-# NOTE: The event arguments don't exist yet! This is merely a sketch of how it will look.
-# We still need to implement an event system!
+These minions are implemented for Hearthstone Path 20.0.
+Data Source: https://hearthstone.fandom.com/wiki/Battlegrounds#Minions_by_tier
+"""
+from typing import Dict
+
+from hsbg.models import CardClass, CardRarity, CardAbility, MinionRace, Buff, Minion
+
+
+def get_all_minions(gold_suffix: str = '_golden') -> Dict[str, Minion]:
+    """Return a dict mapping the name of a minion to its instance.
+    The key is the name of the minion (with an additional suffix if the minion is golden).
+
+    Args:
+        gold_suffix: The suffix to add to keys representing minions that are golden.
+    """
+    all_minions = {}
+    for obj in globals().values():
+        if not isinstance(obj, Minion):
+            continue
+        key = obj.name + (gold_suffix if obj.is_golden else '')
+        all_minions[key] = obj
+    return all_minions
+
 
 ################################################################################
 # Tier 1 cards
 ################################################################################
-ALLEY_CAT = Minion(
-    'Alleycat', MinionType.BEAST, health, 1,
-    keywords=MinionKeyword.BATTLE_CRY,
-    on_played=lambda ctx: ctx.board.summon('Tabbycat', ctx.index, \
-                                           golden: ctx.source_minion.level == 2)
+# Beast Pool
+TABBY_CAT = Minion(
+    'Tabbycat', CardClass.HUNTER, MinionRace.BEAST, 1, 1,
+    purchasable=False
 )
-TABBY_CAT = Minion('Tabbycat', MinionType.BEAST, 1, 1, in_pool=False)
-DIRE_WOLF_ALPHA = Minion(
-    'Dire Wolf Alpha', MinionType.BEAST, 2, 2,
+TABBY_CAT_GOLDEN = Minion(
+    'Tabbycat', CardClass.HUNTER, MinionRace.BEAST, 2, 2,
+    is_golden=True, purchasable=False
+)
+
+ALLEY_CAT = Minion(
+    'Alleycat', CardClass.HUNTER, MinionRace.BEAST, 1, 1,
+    abilities=CardAbility.BATTLECRY | CardAbility.SUMMON,
+    _on_this_played=lambda self, ctx: ctx.board.summon('Tabbycat', is_golden=False)
+)
+ALLEY_CAT_GOLDEN = Minion(
+    'Alleycat', CardClass.HUNTER, MinionRace.BEAST, 2, 2,
+    is_golden=True, abilities=CardAbility.BATTLECRY | CardAbility.SUMMON,
+    _on_this_played=lambda self, ctx: ctx.board.summon('Tabbycat', is_golden=True)
+)
+
+SCAVENGING_HYENA = Minion(
+    'Scavenging Hyena', CardClass.HUNTER, MinionRace.BEAST, 2, 2,
+    cost=2
+)
+SCAVENGING_HYENA_GOLDEN = Minion(
+    'Scavenging Hyena', CardClass.HUNTER, MinionRace.BEAST, 4, 4,
+    cost=2, is_golden=True
+)
+
+# Demon Pool
+# TODO: Deathrattle (Give this minion's attack to a random friendly minion)
+FIENDISH_SERVANT = Minion(
+    'Fiendish Servant', CardClass.WARLOCK, MinionRace.DEMON, 2, 2,
+    abilities=CardAbility.DEATH_RATTLE
+)
+FIENDISH_SERVANT_GOLDEN = Minion(
+    'Fiendish Servant', CardClass.WARLOCK, MinionRace.DEMON, 4, 2,
+    is_golden=True, abilities=CardAbility.DEATH_RATTLE
+)
+
+VULGAR_HOMUNCULUS = Minion(
+    'Vulgar Homunculus', CardClass.WARLOCK, MinionRace.DEMON, 2, 4,
+    cost=2, abilities=CardAbility.TAUNT | CardAbility.BATTLECRY,
+    _on_this_played=lambda self, ctx: ctx.board.attack_hero(2, kind='friendly')
+)
+VULGAR_HOMUNCULUS_GOLDEN = Minion(
+    'Vulgar Homunculus', CardClass.WARLOCK, MinionRace.DEMON, 4, 8,
+    cost=2, is_golden=True, abilities=CardAbility.TAUNT | CardAbility.BATTLECRY,
+    _on_this_played=lambda self, ctx: ctx.board.attack_hero(2, kind='friendly')
+)
+
+def _wrath_weaver_on_any_played(self, ctx) -> None:
+    """Handle the effect for the Wrath Weaver minion when a card is played from the hand.
+    Effect: After you play a demon, deal 1 damage to your hero, and gain +2/+2 (or +4/+4 if golden).
+    """
+    if MinionRace.DEMON not in ctx.played_card.race:
+        return
+    if self.is_golden:
+        buff = Buff(2, 2, CardAbility.NONE)
+    else:
+        buff = Buff(4, 4, CardAbility.NONE)
+
+    ctx.board.attack_hero(1, kind='friendly')
+    self.add_buff(buff)
+
+WRATH_WEAVER = Minion(
+    'Wrath Weaver', CardClass.NEUTRAL, MinionRace.NONE, 1, 3,
+    _on_any_played=_wrath_weaver_on_any_played
+)
+WRATH_WEAVER_GOLDEN = Minion(
+    'Wrath Weaver', CardClass.NEUTRAL, MinionRace.NONE, 2, 6,
+    is_golden=True,
+    _on_any_played=_wrath_weaver_on_any_played
+)
+
+# Dragon Pool
+DRAGON_SPAWN_LIEUTENANT = Minion(
+    'Dragonspawn Lieutenant', CardClass.NEUTRAL, MinionRace.DRAGON, 2, 3,
+    cost=2, abilities=CardAbility.TAUNT
+)
+DRAGON_SPAWN_LIEUTENANT_GOLDEN = Minion(
+    'Dragonspawn Lieutenant', CardClass.NEUTRAL, MinionRace.DRAGON, 4, 6,
+    cost=2, is_golden=True, abilities=CardAbility.TAUNT
+)
+
+# TODO: Start of combat (Deal 1 damage per friendly Dragon to one random enemy minion).
+RED_WHELP = Minion(
+    'Red Whelp', CardClass.NEUTRAL, MinionRace.DRAGON, 1, 2,
+    abilities=CardAbility.START_OF_COMBAT
+)
+RED_WHELP_GOLDEN = Minion(
+    'Red Whelp', CardClass.NEUTRAL, MinionRace.DRAGON, 1, 2,
+    is_golden=True, abilities=CardAbility.START_OF_COMBAT
+)
+
+# Elemental Pool
+REFRESHING_ANOMALY = Minion(
+    'Refreshing Anomaly', CardClass.NEUTRAL, MinionRace.ELEMENTAL, 1, 3,
+    abilities=CardAbility.BATTLECRY,
+    _on_this_played=lambda self, ctx: ctx.modify_refresh_cost(0, times=1)
+)
+REFRESHING_ANOMALY_GOLDEN = Minion(
+    'Refreshing Anomaly', CardClass.NEUTRAL, MinionRace.ELEMENTAL, 2, 6,
+    is_golden=True, abilities=CardAbility.BATTLECRY,
+    _on_this_played=lambda self, ctx: ctx.modify_refresh_cost(0, times=2)
+)
+
+WATER_DROPLET = Minion(
+    'Water Droplet', CardClass.NEUTRAL, MinionRace.ELEMENTAL, 2, 2,
+    cost=3, purchasable=False
+)
+WATER_DROPLET_GOLDEN = Minion(
+    'Water Droplet', CardClass.NEUTRAL, MinionRace.ELEMENTAL, 4, 4,
+    cost=3, purchasable=False, is_golden=True
+)
+
+SELLEMENTAL = Minion(
+    'Sellemental', CardClass.NEUTRAL, MinionRace.ELEMENTAL, 2, 2,
+    cost=3, abilities=CardAbility.GENERATE,
+    _on_this_sold=lambda self, ctx: ctx.board.add_card_to_hand('Water Droplet', num=1)
+)
+SELLEMENTAL_GOLDEN = Minion(
+    'Sellemental', CardClass.NEUTRAL, MinionRace.ELEMENTAL, 2, 2,
+    cost=3, abilities=CardAbility.GENERATE, is_golden=True,
+    _on_this_sold=lambda self, ctx: ctx.board.add_card_to_hand('Water Droplet', num=2)
+)
+
+# Mech Pool
+MICRO_MACHINE = Minion(
+    'Micro Machine', CardClass.NEUTRAL, MinionRace.MECH, 1, 2,
     cost=2,
-    on_played=lambda ctx: ctx.board.buff_adjacent(ctx.source_minion, attack=1, aura=True)
+    _on_new_turn=lambda self, ctx: self.add_buff(Buff(1, 0, CardAbility.NONE))
+)
+MICRO_MACHINE_GOLDEN = Minion(
+    'Micro Machine', CardClass.NEUTRAL, MinionRace.MECH, 2, 4,
+    cost=2, is_golden=True,
+    _on_new_turn=lambda self, ctx: self.add_buff(Buff(2, 0, CardAbility.NONE))
+)
+
+def _micro_mummy_on_end_turn(self, ctx) -> None:
+    """Handle the Micro Mummy reborn effect on the end of a turn.
+    Effect: At the end of your turn, give another random friendly
+    minion +1 (or +2 if golden) Attack.
+    """
+    minion = ctx.board.get_random_minion(kind='friendly')
+    if minion == self or minion is None:
+        return
+    if self.is_golden:
+        minion.add_buff(Buff(2, 0, CardAbility.NONE))
+    else:
+        minion.add_buff(Buff(1, 0, CardAbility.NONE))
+
+MICRO_MUMMY = Minion(
+    'Micro Mummy', CardClass.PALADIN, MinionRace.MECH, 1, 2,
+    cost=2, rarity=CardRarity.EPIC, abilities=CardAbility.REBORN,
+    _on_end_turn=_micro_mummy_on_end_turn
+)
+MICRO_MUMMY_GOLDEN = Minion(
+    'Micro Mummy', CardClass.PALADIN, MinionRace.MECH, 2, 4,
+    cost=2, rarity=CardRarity.EPIC, is_golden=True, abilities=CardAbility.REBORN,
+    _on_end_turn=_micro_mummy_on_end_turn
+)
+
+# Murloc Pool
+def _murloc_tidecaller_on_card_summoned(self, ctx) -> None:
+    """Handle the Murloc Tidecaller effect.
+    Effect: Whenever you summon a Murloc, gain +1 (or +2 if golden) Attack."""
+    if MinionRace.MURLOC not in ctx.summoned_card.race:
+        return
+    if self.is_golden:
+        self.add_buff(Buff(2, 0, CardAbility.NONE))
+    else:
+        self.add_buff(Buff(1, 0, CardAbility.NONE))
+
+MURLOC_TIDECALLER = Minion(
+    'Murloc Tidecaller', CardClass.NEUTRAL, MinionRace.MURLOC, 1, 2,
+    rarity=CardRarity.RARE,
+    _on_card_summoned=_murloc_tidecaller_on_card_summoned
+)
+MURLOC_TIDECALLER_GOLDEN = Minion(
+    'Murloc Tidecaller', CardClass.NEUTRAL, MinionRace.MURLOC, 2, 4,
+    rarity=CardRarity.RARE, is_golden=True,
+    _on_card_summoned=_murloc_tidecaller_on_card_summoned
+)
+
+MURLOC_TIDEHUNTER = Minion(
+    'Murloc Tidehunter', CardClass.NEUTRAL, MinionRace.MURLOC, 2, 1,
+    abilities=CardAbility.BATTLECRY | CardAbility.SUMMON,
+    _on_this_played=lambda self, ctx: ctx.board.summon('Murloc Scout', is_golden=False)
+)
+MURLOC_TIDEHUNTER_GOLDEN = Minion(
+    'Murloc Tidehunter', CardClass.NEUTRAL, MinionRace.MURLOC, 4, 2,
+    cost=2, is_golden=True, abilities=CardAbility.BATTLECRY | CardAbility.SUMMON,
+    _on_this_played=lambda self, ctx: ctx.board.summon('Murloc Scout', is_golden=True)
+)
+
+def _rockpool_hunter_on_this_played(self, ctx) -> None:
+    """Handle the Rockpool Hunter battlecry effect.
+    Effect: Give a friendly Murloc +1/+1 (or +2/+2 if golden).
+
+    Note: the murloc is chosen RANDOMLY since we do not have targetting implemented.
+    """
+    # Choose a random friendly Murloc
+    minion = ctx.board.get_random_minion(race=MinionRace.MURLOC, kind='friendly')
+    if minion is None:
+        return
+
+    if self.is_golden:
+        minion.add_buff(Buff(2, 2, CardAbility.NONE))
+    else:
+        minion.add_buff(Buff(1, 1, CardAbility.NONE))
+
+ROCKPOOL_HUNTER = Minion(
+    'Rockpool Hunter', CardClass.NEUTRAL, MinionRace.MURLOC, 2, 3,
+    cost=2, abilities=CardAbility.BATTLECRY,
+    _on_this_played=_rockpool_hunter_on_this_played
+)
+ROCKPOOL_HUNTER_GOLDEN = Minion(
+    'Rockpool Hunter', CardClass.NEUTRAL, MinionRace.MURLOC, 4, 6,
+    cost=2, is_golden=True, abilities=CardAbility.BATTLECRY,
+    _on_this_played=_rockpool_hunter_on_this_played
+)
+
+# Pirate Pool
+def _deck_swabbie_on_this_played(self, ctx) -> None:
+    """Handle the Deck Swabbie battlecry effect.
+    Effect: Reduce the cost of upgrading Bob's Tavern by (1) (or (2) if golden).
+    """
+    cost = ctx.board.current_tavern_upgrade_cost
+    discount = 2 if self.is_golden else 1
+    ctx.board.modify_tavern_upgrade_cost(cost - discount, times=1)
+
+DECK_SWABBIE = Minion(
+    'Deck Swabbie', CardClass.NEUTRAL, MinionRace.PIRATE, 2, 2,
+    cost=3, abilities=CardAbility.BATTLECRY,
+    _on_this_played=_deck_swabbie_on_this_played
+)
+DECK_SWABBIE_GOLDEN = Minion(
+    'Deck Swabbie', CardClass.NEUTRAL, MinionRace.PIRATE, 4, 4,
+    cost=3, is_golden=True, abilities=CardAbility.BATTLECRY,
+    _on_this_played=_deck_swabbie_on_this_played
+)
+
+# TODO: Deathrattle (Summon a 1/1 Pirate. It attacks immediately.)
+SCALLYWAG = Minion(
+    'Scallywag', CardClass.NEUTRAL, MinionRace.PIRATE, 2, 1,
+    abilities=CardAbility.DEATH_RATTLE
+)
+# TODO: Deathrattle (Summon a 2/2 Pirate. It attacks immediately.)
+SCALLYWAG_GOLDEN = Minion(
+    'Scallywag', CardClass.NEUTRAL, MinionRace.PIRATE, 4, 2,
+    is_golden=True, abilities=CardAbility.DEATH_RATTLE
+)
+
+# Neutral Pool
+ACOLYTE_OF_CTHUN = Minion(
+    'Acolyte of C\'Thun', CardClass.NEUTRAL, MinionRace.NEUTRAL, 2, 2,
+    abilities=CardAbility.TAUNT | CardAbility.REBORN
+)
+ACOLYTE_OF_CTHUN_GOLDEN = Minion(
+    'Acolyte of C\'Thun', CardClass.NEUTRAL, MinionRace.NEUTRAL, 4, 4,
+    is_golden=True, abilities=CardAbility.TAUNT | CardAbility.REBORN
+)
+
+
+################################################################################
+# Tier 2 cards
+################################################################################
+BIG_BAD_WOLF = Minion(
+    'Big Bad Wolf', CardClass.HUNTER, MinionRace.BEAST, 3, 2,
+    cost=2, tier=1
+)
+BIG_BAD_WOLF_GOLDEN = Minion(
+    'Big Bad Wolf', CardClass.HUNTER, MinionRace.BEAST, 6, 4,
+    cost=2, tier=1, is_golden=True
+)
+
+# TODO: Deathrattle (Summon a 3/2 Big Bad Wolf.)
+KINDLY_GRANDMOTHER = Minion(
+    'Kindly Grandmother', CardClass.HUNTER, MinionRace.BEAST, 1, 1,
+    cost=2, tier=2, abilities=CardAbility.DEATH_RATTLE
+)
+# TODO: Deathrattle (Summon a 6/4 Big Bad Wolf.)
+KINDLY_GRANDMOTHER_GOLDEN = Minion(
+    'Kindly Grandmother', CardClass.HUNTER, MinionRace.BEAST, 2, 2,
+    cost=2, tier=2, is_golden=True, abilities=CardAbility.DEATH_RATTLE
 )
--- hsbg/models.py
+++ hsbg/models.py
@@ -3,59 +3,28 @@ from __future__ import annotations
 
 import copy
 from enum import Enum, Flag, auto
+from typing import List, Optional
 from dataclasses import dataclass, field
-from typing import Tuple, List, Set, Dict
 from abc import ABC, abstractproperty, abstractmethod
 
-class MinionAbility(Flag):
-    """A special affect, power, or behaviour found on cards."""
-    NONE = 0
-    TAUNT = auto()
-    DIVINE_SHIELD = auto()
-    POISON = auto()
-    WIND_FURY = auto()
+
+class CardClass(Enum):
+    """The class of a card."""
+    DEMON_HUNTER = 'demon_hunter'
+    DRUID = 'druid'
+    HUNTER = 'hunter'
+    MAGE = 'mage'
+    PALADIN = 'paladin'
+    PRIEST = 'priest'
+    ROGUE = 'rogue'
+    SHAMAN = 'shaman'
+    WARLOCK = 'warlock'
+    WARRIOR = 'warrior'
+    DEATH_KNIGHT = 'death_knight'
+    NEUTRAL = 'neutral'
 
 
-class MinionType(Flag):
-    """The type of the minion."""
-    NONE = 0
-    BEAST = auto()
-    DEMON = auto()
-    DRAGON = auto()
-    MECH = auto()
-    MURLOC = auto()
-    NEUTRAL = auto()
-    AMALGAM = BEAST | DEMON | DRAGON | MECH | MURLOC
-    ALL = ~0
-
-
-class MinionKeyword(Flag):
-    """A keyword on a minion card."""
-    NONE = 0
-    TARGETED = auto()
-    PLAYER_IMMUNITY = auto()
-    BATTLE_CRY = auto()
-    MAGNETIC = auto()
-    DEATH_RATTLE = auto()
-    SPECIAL_ATTACK = auto()
-
-
-class AdaptationBonus(Flag):
-    """An adaptation bonus.
-    Adapt is an ability which allows the player to choose one of the three bonuses,
-    called adaptations, for a minion from a total of ten choice cards.
-    """
-    ONE_ONE = auto()
-    THREE_HEALTH = auto()
-    THREE_ATTACK = auto()
-    DIVINE_SHIELD = auto()
-    TAUNT = auto()
-    POSION = auto()
-    DEATH_RATTLE = auto()
-    WIND_FURY = auto()
-
-
-class Rarity(Enum):
+class CardRarity(Enum):
     """The rarity of a card."""
     CLASSIC = 'classic'
     COMMON = 'common'
@@ -64,35 +33,45 @@ class Rarity(Enum):
     LEGENDARY = 'legendary'
 
 
-class AuraType(Enum):
-    """The type of the aura effect.
-    An aura is an ongoing effect which grants temporary enchantments or buffs to other minions."""
-    BATTLE_CRY = 'battle_cry'
-    SUMMON = 'summon'
-    DEATH_RATTLE = 'death_rattle'
+class CardAbility(Flag):
+    """A special affect, power, or behaviour found on cards."""
+    NONE = 0
+    TAUNT = auto()
+    DIVINE_SHIELD = auto()
+    POISON = auto()
+    WINDFURY = auto()
+    BATTLECRY = auto()
+    DEATH_RATTLE = auto()
+    START_OF_COMBAT = auto()
+    SUMMON = auto()
+    GENERATE = auto()
+    REBORN = auto()
 
 
-@dataclass
-class MinionTier:
-    """The tier of a minion.
+class MinionRace(Flag):
+    """The race of the minion."""
+    NONE = 0
+    BEAST = auto()
+    DEMON = auto()
+    DRAGON = auto()
+    MECH = auto()
+    MURLOC = auto()
+    ELEMENTAL = auto()
+    PIRATE = auto()
+    NEUTRAL = auto()
+    AMALGAM = BEAST | DEMON | DRAGON | MECH | MURLOC
+    ALL = ~0
 
-    Instance Attributes:
-        - tier: The value of the tier.
-        - num_copies: The number of copies of a minion with this tier.
-    """
-    tier: int = 1
-    num_copies: int = 1
 
-
-# A dict mapping each tier rank to a MinionTier instance.
-TIER_RANKS = {
-    1: MinionTier(1, 18),
-    2: MinionTier(2, 15),
-    3: MinionTier(3, 13),
-    4: MinionTier(4, 11),
-    5: MinionTier(5, 9),
-    6: MinionTier(6, 6)
-}
+# class MinionKeyword(Flag):
+#     """A keyword on a minion card."""
+#     NONE = 0
+#     TARGETED = auto()
+#     PLAYER_IMMUNITY = auto()
+#     BATTLECRY = auto()
+#     MAGNETIC = auto()
+#     DEATH_RATTLE = auto()
+#     SPECIAL_ATTACK = auto()
 
 
 @dataclass
@@ -114,112 +93,75 @@ class Minion:
     """A minion.
 
     Instance Attributes:
-        - name: The name of the card.
-        - rarity: The rarity of the card.
-        - cost: The mana cost of the card.
-        - type: The type of the minion.
-        - tier: The tier of this minion.
-        - abilities: The abilities of this minion.
-                     This consists of a combination of the MinionAbility flags.
-        - keywords: The keywords of this minion.
-                    This consists of a combination of the MinionKeyword flags.
-        - valid_targets: The type of minions this minion can target.
-        - level: The level of this minion.
+        - name: The name of this minion.
+        - card_class: The type of this minion.
+        - race: The race of this minion.
         - health: The health of this minion.
         - attack: The attack of this minion.
-        - in_pool: Whether this minion is in the pool.
-        - parent_minion: The parent of this minion (used when a minion creates a new one).
+        - cost: The mana cost of this minion.
+        - rarity: The rarity of this minion.
+        - tier: The tier of this minion.
+        - is_golden: Whether this minion is a golden copy.
+        - abilities: The abilities of this minion.
+                     This consists of a combination of the CardAbility flags.
+        - purchasable: Whether this minion is in the card pool (i.e. can it be purchased).
     """
     # Private Instance Attributes:
-    #   - _damage_taken: The amount of damage the minion has taken.
-    #   - _temp_buffs: A set of buffs appplied to this minion.
+    #   - _buffs: A set of buffs appplied to this minion.
+    #   - _on_this_bought: A function called when this minion is bought.
+    #   - _on_this_sold: A function called when this minion is sold.
+    #   - _on_this_played: A function called when this minion is played from the hand.
+    #   - _on_any_played: A function called when any card is played from the hand.
+    #   - _on_card_summoned: A function called when a card is summoned.
+    #   - _on_new_turn: A function called on the start of a new turn.
+    #   - _on_end_turn: A function called on the start of a new turn.
     name: str
-    type: MinionType
+    card_class: CardClass
+    race: MinionRace
     health: int
     attack: int
-
     cost: int = 1
-    rarity: Rarity = Rarity.COMMON
-    tier: MinionTier = TIER_RANKS[1]
-    level: int = 1
+    rarity: CardRarity = CardRarity.COMMON
+    tier: int = 1
 
-    abilities: MinionAbility = MinionAbility.NONE
-    keywords: MinionKeyword = MinionKeyword.NONE
-    valid_targets: MinionType = MinionType.ALL
+    is_golden: bool = False
+    abilities: MinionAbility = CardAbility.NONE
 
-    in_pool: bool = True
-    parent_minion: Optional[Minion] = None
+    purchasable: bool = True
+    _buffs: List[Buff] = field(default_factory=list)
 
-    _damage_taken: int = 0
-    _temp_buffs: List[Buff] = field(default_factory=list)
-
-    # TODO: Add event system for minion callbacks (i.e. on death, on turn start, etc...)
+    # Events
+    _on_this_bought: Optional[callable] = None
+    _on_this_sold: Optional[callable] = None
+    _on_this_played: Optional[callable] = None
+    _on_any_played: Optional[callable] = None
+    _on_card_summoned: Optional[callable] = None
+    _on_new_turn: Optional[callable] = None
+    _on_end_turn: Optional[callable] = None
 
     @property
     def current_health(self) -> int:
-        """Return the current health of this minion."""
-        total_health = self.health + sum(buff.health for buff in self._temp_buffs)
-        return total_health - self._damage_taken
+        """Return the current health of this minion (including buffs)."""
+        total_health = self.health + sum(buff.health for buff in self._buffs)
+        return total_health
 
     @property
     def current_attack(self) -> int:
-        """Return the current attack of this minion."""
-        return self.attack + sum(buff.attack for buff in self._temp_buffs)
+        """Return the current attack of this minion (including buffs)."""
+        return self.attack + sum(buff.attack for buff in self._buffs)
 
     @property
     def current_abilities(self) -> MinionAbility:
-        """Return the current abilities of this minion."""
+        """Return the current abilities of this minion (including buffs)."""
         current_abilities = self.abilities
-        for buff in self._temp_buffs:
+        for buff in self._buffs:
             current_abilities |= buff.abilities
         return current_abilities
-
-    @property
-    def is_dead(self) -> bool:
-        """Return whether this minion is dead."""
-        return self.current_health <= 0
-
-    @property
-    def has_overkilled(self) -> bool:
-        """Return whether this minion has overkilled (i.e. health is negative)."""
-        return self.current_health < 0
-
-    def take_damage(self, damage: int) -> Tuple[bool, bool, bool, bool]:
-        """Inflict the given amount of damage onto this minion.
-
-        Return a 4-tuple of booleans values consisting of whether this minion
-        took damage, lost divine shield, overkilled, and/or whether this minion is dead.
-
-        >>> minion = Minion('Mario', MinionType.NEUTRAL, 3, 1)  # Create a minion with 3 health.
-        >>> minion.abilities |= MinionAbility.DIVINE_SHIELD  # Give the minion divine shield
-        >>> minion.take_damage(100)
-        (False, True, False, False)
-        >>> minion.current_health
-        3
-        >>> minion.take_damage(2)
-        (True, False, False, False)
-        >>> minion.current_health
-        1
-        >>> minion.take_damage(2)
-        (True, False, True, True)
-        >>> minion.current_health
-        -1
-        """
-        if damage == 0:
-            return False, False, False, False
-
-        if MinionAbility.DIVINE_SHIELD in self.abilities:
-            # Clear divine shield, since we've taken damage.
-            self.abilities &= ~MinionAbility.DIVINE_SHIELD
-            return False, True, False, False
-
-        self._damage_taken += damage
-        return True, False, self.has_overkilled, self.is_dead
 
     def add_buff(self, buff: Buff) -> None:
         """Apply the given buff to this minion whose source is the given minion.
 
-        >>> minion = Minion('Lonely Boy', MinionType.DEMON, 0, 0)  # A lonely minion.
+        >>> minion = Minion('Lonely Boy', MinionRace.DEMON, 0, 0)  # A lonely minion.
         >>> buff = Buff(health=1, attack=2, abilities=MinionAbility.TAUNT |\
                                                       MinionAbility.DIVINE_SHIELD)
         >>> minion.add_buff(buff)
@@ -229,13 +171,13 @@ class Minion:
             MinionAbility.DIVINE_SHIELD in minion.current_abilities
         True
         """
-        self._temp_buffs.append(buff)
+        self._buffs.append(buff)
 
     def remove_buff(self, buff: Buff) -> None:
         """Remove the given buff.
         Do nothing if the given buff is not applied to this minion.
 
-        >>> minion = Minion('Lonely Boy', MinionType.DEMON, 0, 0)  # A lonely minion.
+        >>> minion = Minion('Lonely Boy', MinionRace.DEMON, 0, 0)  # A lonely minion.
         >>> buff = Buff(health=1, attack=2, abilities=MinionAbility.TAUNT |\
                                                       MinionAbility.DIVINE_SHIELD)
         >>> minion.add_buff(buff)
@@ -247,8 +189,7 @@ class Minion:
         """
         if buff not in self._temp_buffs:
             return
-        self._damage_taken = max(self._damage_taken - buff.health, 0)
-        self._temp_buffs.remove(buff)
+        self._buffs.remove(buff)
 
     def clone(self, keep_buffs: bool = False) -> Minion:
         """Return a clone of this minion.
@@ -256,7 +197,7 @@ class Minion:
         Args:
             keep_buffs: Whether to keep the buffs applied to this minion.
 
-        >>> minion = Minion('Lonely Boy', MinionType.DEMON, 0, 0)  # A lonely minion.
+        >>> minion = Minion('Lonely Boy', MinionRace.DEMON, 0, 0)  # A lonely minion.
         >>> copy_minion = minion.clone()
         >>> minion is copy_minion
         False
@@ -264,8 +205,22 @@ class Minion:
         minion_copy = copy.copy(self)
         if not keep_buffs:
             # Clear buffs
-            minion_copy._temp_buffs = dict()
+            minion_copy._buffs = dict()
         return minion_copy
+
+    def on_this_played(self, ctx: Any) -> None:
+        """Handle when THIS minion is played from the hand.
+        Mutates the given game state.
+        """
+        if self._on_this_played is not None:
+            self._on_this_played(self, ctx)
+
+    def on_any_played(self, ctx: Any) -> None:
+        """Handle when ANY minion is played from the hand.
+        Mutates the given game state.
+        """
+        if self._on_any_played is not None:
+            self._on_any_played(self, ctx)
 
 
 if __name__ == '__main__':
