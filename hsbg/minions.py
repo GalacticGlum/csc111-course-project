e49f4c13e737bcf6bcf37988276a1cd66ae1a1ed
GalacticGlum <nerftactic09@gmail.com>
2021-04-02 02:08:12 -0400

    Modified module docstring for hsbg.minions

--- hsbg/minions.py
+++ hsbg/minions.py
@@ -7,9 +7,8 @@ NOTE: This is a collection of all minions in the Battlegrounds pool as of Patch 
       However, not all of these minions are implemented. They are merely defined here
       in case that they should be implemented in the future.
 
-      Refer to the implemented_minions.txt file for a full list of all minions with
-      implementations in the Python recruitment phase simulator, and the C++ combat phase
-      simulator.
+      Refer to the minion_list.txt file for a full list of all minions with implementations
+      in the Python recruitment phase simulator, and the C++ combat phase simulator.
 """
 from __future__ import annotations
 import random
