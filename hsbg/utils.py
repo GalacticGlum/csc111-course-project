d0cc00153e4f933dcae1d19522786a8af4510a61
GalacticGlum <nerftactic09@gmail.com>
2021-04-02 00:34:27 -0400

    Added make_frequency_table function to utils.py

--- hsbg/utils.py
+++ hsbg/utils.py
@@ -1,6 +1,6 @@
 """Utility functions."""
 from __future__ import annotations
-from typing import Iterable, List
+from typing import Iterable, List, Dict, Optional, Callable, Any
 
 
 def filter_minions(minions: Iterable[Minion], clone: bool = False, limit: Optional[int] = None,
@@ -27,3 +27,33 @@ def filter_minions(minions: Iterable[Minion], clone: bool = False, limit: Option
             matches.append(minion)
 
     return matches
+
+
+def make_frequency_table(values: List[Any], key: Optional[Callable[[Any], Any]] = None) \
+        -> Dict[Any, int]:
+    """Return a dict mapping the frequency of each distinct element in the given list.
+
+    Args:
+        values: A list of values.
+        key: A function which takes in as input an element of the list, and returns some value,
+             which is then used as a key to the frequency table.
+
+    >>> values = [1, 3, 3, 5, 4, 3]
+    >>> make_frequency_table(values) == {1: 1, 3: 3, 4: 1, 5: 1}
+    True
+    >>> make_frequency_table(values, key=lambda x: 2 * x) == {2: 1, 6: 3, 8: 1, 10: 1}
+    True
+    """
+    table = {}
+    for x in values:
+        if key is not None:
+            x = key(x)
+        if x not in table:
+            table[x] = 0
+        table[x] += 1
+    return table
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
