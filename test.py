02fc6320d373d8308f36a76790c523b3288222ee
GalacticGlum <nerftactic09@gmail.com>
2021-04-11 16:12:53 -0400

    Updated test.py

--- test.py
+++ test.py
@@ -2,14 +2,15 @@ import time
 import pprint
 from hsbg.ai import RandomPlayer, run_game, run_games
 
-# start_time=time.time()
-# winner, move_sequence = run_game([RandomPlayer(), RandomPlayer()])
-# print(f'Player {winner + 1} won the game!')
-# # pprint.pprint(move_sequence)
-# print('Took {:.2f} seconds to run a game'.format(time.time() - start_time))
+if __name__ == '__main__':
+    # start_time=time.time()
+    # winner, move_sequence = run_game([RandomPlayer(), RandomPlayer()])
+    # print(f'Player {winner + 1} won the game!')
+    # # pprint.pprint(move_sequence)
+    # print('Took {:.2f} seconds to run a game'.format(time.time() - start_time))
 
-start_time=time.time()
-n = 64
-run_games(n, [RandomPlayer(), RandomPlayer()], n_jobs=8)
-elapsed = time.time() - start_time
-print('Took {:.3f} seconds to run {} games ({:.3f} seconds per game)'.format(elapsed, n, elapsed / n))
+    start_time=time.time()
+    n = 1000
+    run_games(n, [RandomPlayer(), RandomPlayer()], n_jobs=8)
+    elapsed = time.time() - start_time
+    print('Took {:.3f} seconds to run {} games ({:.3f} seconds per game)'.format(elapsed, n, elapsed / n))
