b093e829bb1f6e7e02d853307749c9ac298d5638
GalacticGlum <nerftactic09@gmail.com>
2021-04-14 20:32:55 -0400

    Added hearthstone wiki scraping and cleaning tools

--- hsbg/utils.py
+++ hsbg/utils.py
@@ -1,6 +1,9 @@
 """Utility functions."""
 from __future__ import annotations
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import Any, Iterable, Tuple, List, Dict, Callable, Optional, Union
+
+from tqdm import tqdm
 
 
 def filter_minions(minions: Iterable[Minion], clone: bool = False, limit: Optional[int] = None,
@@ -69,6 +72,99 @@ def make_frequency_table(values: List[Any], key: Optional[Callable[[Any], Any]] 
             table[x] = tuple(table[x])
 
     return table
+
+
+def parallel_map(iterables: Union[list, iter], function: callable, n_jobs: Optional[int] = 16,
+                 use_kwargs: Optional[bool] = False, front_num: Optional[int] = 3,
+                 show_progress_bar: Optional[bool] = True, initial_value: Optional[list] = None,
+                 raise_errors: Optional[bool] = False, include_errors: Optional[bool] = True,
+                 extend_result: Optional[bool] = False, return_output: Optional[bool] = True,
+                 add_func: Optional[callable] = None) -> Union[list, None]:
+    """A parallel version of the map function with a progress bar.
+    Return a list of the form [function(iterables[0]), function(iterables[1]), ...].
+    Args:
+        iterables: A sequence, collection, or iterator object.
+        function: A function to apply to the elements of `iterables`.
+        n_jobs: The number of jobs to run.
+        use_kwargs: Whether to consider the elements of iterables as dictionaries of
+            keyword arguments to function.
+        front_num: The number of iterations to run serially before dispatching the
+            parallel jobs. Useful for catching exceptions.
+        show_progress_bar: Whether to show a progress bar while the jobs run.
+        initial_value: The initial value of the output list.
+            This should be an iterables-like object.
+        raise_errors: Whether to raise errors.
+        include_errors: Whether to include the errors in the output list.
+        extend_result: Whether the resultant list should be extended rather than appended to.
+        return_output: Whether to return a list containing the output values of the function.
+            If False, this function does not return None.
+        add_func: A custom function for adding the output values of the function to the result list.
+            This function has two parameters, the value to add and the list to add it to, and it
+            should mutate the list.
+    Preconditions:
+        - n_jobs >= 1
+        - front_num >= 0
+    """
+    if isinstance(iterables, list):
+        front = [function(**a) if use_kwargs else function(a) for a in iterables[:front_num]]
+        iterables = iterables[front_num:]
+    else:
+        front = []
+        for _ in range(front_num):
+            a = next(iterables)
+            front.append(function(**a) if use_kwargs else function(a))
+
+    def _add_func(x: object, output: list) -> None:
+        """Add a value to the output list."""
+        # No reason to add if we aren't returning the output.
+        if not return_output:
+            return
+
+        if add_func is not None:
+            add_func(x, output)
+        else:
+            if extend_result:
+                output.extend(x)
+            else:
+                output.append(x)
+
+    output = initial_value or list()
+    for x in front:
+        _add_func(x, output)
+
+    # If n_jobs == 1, then we are not parallelising, run all elements serially.
+    if n_jobs == 1:
+        for a in tqdm(iterables):
+            x = function(**a) if use_kwargs else function(a)
+            _add_func(x, output)
+
+        return output if return_output else None
+
+    with ThreadPoolExecutor(max_workers=n_jobs) as pool:
+        futures = [
+            pool.submit(function, **a) if use_kwargs else
+            pool.submit(function, a) for a in iterables
+        ]
+
+        for _ in tqdm(as_completed(futures), total=len(futures), unit='it',
+                      unit_scale=True, disable=not show_progress_bar):
+            # Do nothing...This for loop is just here to iterate through the futures
+            pass
+
+    # Don't bother retrieving the results from the future...If we don't return anything.
+    if not return_output:
+        return None
+
+    for _, future in tqdm(enumerate(futures)):
+        try:
+            _add_func(future.result(), output)
+        except Exception as exception:
+            if raise_errors:
+                raise exception
+            if include_errors:
+                _add_func(exception, output)
+
+    return output
 
 
 if __name__ == '__main__':
---
+++ scripts/hearthstone_wiki.py
@@ -0,0 +1,207 @@
+"""Tools for scraping, cleaning, and processing pages from the Hearthstone wiki."""
+import re
+import json
+import requests
+import argparse
+from pathlib import Path
+from typing import Tuple, List, Optional
+from urllib.parse import quote as url_quote
+
+import click
+import pypandoc
+import contractions
+from tqdm import tqdm
+
+from hsbg.utils import parallel_map
+
+
+# Base api url for the Hearthstone wiki
+API_URL = 'https://hearthstone.fandom.com/api.php?action={action}&format=json'
+# A regex pattern to find hypenated words.
+HYPENATED_WORDS_PATTERN = re.compile(r'(\w+)(-)(\w+)')
+
+def request_from_api(params: dict) -> dict:
+    """Perform an api request and return the json response."""
+    url = API_URL.format(**params)
+    request = requests.get(url, params=params)
+    try:
+        return request.json()
+    except ValueError:
+        raise ValueError('Your request to the url "{url}" with the paramaters'
+                         '"{params}" returned data in a format other than JSON.'
+                         'Please check your input data.'.format(url=url, params=params))
+
+
+def get_pages(from_page: Optional[str] = '') -> Tuple[List[dict], Optional[str]]:
+    """Return a list of dicts containing two keys: the id of the page (``id``), and
+    the title of the page (``title``), and the page to continue from, if any.
+
+    Args:
+        from_page: The title of the page to start enumerating from.
+    """
+    params = {
+        'action': 'query',
+        'list': 'allpages',
+        'aplimit': 'max',
+        'apfrom': from_page or '',
+    }
+
+    request = request_from_api(params)
+    pages = [{
+        'id': page['pageid'],
+        'title': page['title']
+    } for page in request['query']['allpages']]
+
+    try:
+        next_page = request['continue']['apcontinue']
+    except:
+        next_page = None
+
+    return pages, next_page
+
+
+def get_page_wikitext(page_id: int) -> str:
+    """Return the wikitext for the page with the given id."""
+    params = {
+        'action': 'parse',
+        'pageid': page_id,
+        'formatversion': 2,
+        'prop': 'wikitext'
+    }
+
+    request = request_from_api(params)
+    try:
+        return request['parse']['wikitext']
+    except:
+        raise ValueError(f'Failed to retrieve wikitext for page with id "{page_id}".')
+
+
+def scrape_pages(output_directory: Path) -> None:
+    """Scrape pages from the Hearthstone wiki and save them in the given directory."""
+    output_directory.mkdir(parents=True, exist_ok=True)
+
+    from_page = ''
+    while from_page is not None:
+        pages, from_page = get_pages(from_page)
+        for page in tqdm(pages):
+            page_id = page['id']
+            wikitext = get_page_wikitext(page_id)
+            filepath = output_directory / f'{page_id}_wikitext.txt'
+            with open(filepath, 'w+', encoding='utf-8') as fp:
+                fp.write(wikitext)
+
+
+def clean_pages(directory: Path, glob_pattern: Optional[str] = None, num_workers: int = 4) \
+        -> None:
+    """Clean the wikitext data. The given directory should only contain text
+    files with wikitext data.
+
+    NOTE: This is an IRREVERSIBLE operation, and will remove files!
+    """
+    pattern = '**/*.txt' or glob_pattern
+    files = list(directory.glob(pattern))
+    total_files = len(files)
+
+    def _clean_file(filepath: Path) -> None:
+        """Clean the given file."""
+        text = filepath.read_text(encoding='utf-8')
+        # If the page starts with a REDIRECT or only has a few words, then just ignore it!
+        if text.startswith('#REDIRECT') or len(text.split()) <= 10:
+            filepath.unlink()
+
+    parallel_map(files, _clean_file, n_jobs=num_workers)
+    remaining_files = len(list(directory.glob(pattern)))
+    print('Removed {} files'.format(total_files - remaining_files))
+
+
+def convert_pages(directory: Path, output_directory: Optional[Path] = None,
+                  glob_pattern: Optional[str] = None, num_workers: int = 4) \
+        -> None:
+    """Convert wikitext data into raw text with pandoc. The given directory should only
+    contain text files with wikitext data.
+    """
+    # Get files
+    pattern = '**/*.txt' or glob_pattern
+    files = list(directory.glob(pattern))
+
+    output_directory = output_directory or directory
+    output_directory.mkdir(parents=True, exist_ok=True)
+
+    def _convert_file(filepath: Path) -> None:
+        """Convert the given file."""
+        output_filepath = output_directory / (filepath.stem + '_converted.txt')
+        # Convert from wikitext to txt
+        pypandoc.convert_file(
+            str(filepath),
+            'plain',
+            format='mediawiki',
+            outputfile=str(output_filepath)
+        )
+
+    parallel_map(files, _convert_file, n_jobs=num_workers)
+
+
+def make_corpus_from_pages(directory: Path, output_filepath: Path,
+                           glob_pattern: Optional[str] = None) -> None:
+    """Make a Hearthstone wiki corpus from converted wikitext files."""
+    pattern = '**/*.txt' or glob_pattern
+    files = list(directory.glob(pattern))
+    with open(output_filepath, 'w+', encoding='utf-8') as fp:
+        for file in tqdm(files):
+            text = file.read_text(encoding='utf-8')
+            # Expand contractions
+            text = contractions.fix(text)
+            # Replace hyphenated words
+            text = re.sub(HYPENATED_WORDS_PATTERN, r'\1_\3', text)
+            # Output text to corpus file
+            fp.write(text + '\n')
+
+
+if __name__ == '__main__':
+    @click.group()
+    def cli() -> None:
+        """Tools for scraping, cleaning, and processing pages from the Hearthstone wiki."""
+
+    @cli.command()
+    @click.argument('output_directory', type=Path)
+    def scrape(output_directory: Path) -> None:
+        """Scrape pages from the Hearthstone wiki."""
+        scrape_pages(output_directory)
+
+    @cli.command()
+    @click.argument('directory', type=Path)
+    @click.option('--glob_pattern', '-g', type=str, default=None)
+    @click.option('--num_workers', '-w', type=int, default=8)
+    def clean(directory: Path, glob_pattern: Optional[str], num_workers: int) -> None:
+        """Clean the wikitext data. The given directory should only contain text
+        files with wikitext data.
+
+        NOTE: This is an IRREVERSIBLE operation, and will remove files!
+        """
+        message = 'This is an IRREVERSIBLE operation, and will remove files! '\
+                  'Do you want to continue'
+        if click.confirm(message, default=True):
+            clean_pages(directory, glob_pattern=glob_pattern, num_workers=num_workers)
+
+    @cli.command()
+    @click.argument('directory', type=Path)
+    @click.option('--output-directory', '-o', type=Path, default=None)
+    @click.option('--glob_pattern', '-g', type=str, default=None)
+    @click.option('--num_workers', '-w', type=int, default=8)
+    def convert(directory: Path, output_directory: Optional[Path],
+                glob_pattern: Optional[str], num_workers: int) -> None:
+        """Convert wikitext data into raw text with pandoc. The given directory should only
+        contain text files with wikitext data.
+        """
+        convert_pages(directory, output_directory, glob_pattern=glob_pattern, num_workers=num_workers)
+
+    @cli.command()
+    @click.argument('directory', type=Path)
+    @click.argument('output_filepath', type=Path)
+    @click.option('--glob_pattern', '-g', type=str, default=None)
+    def make_corpus(directory: Path, output_filepath: Path, glob_pattern: Optional[str]) -> None:
+        """Make a Hearthstone wiki corpus from converted wikitext files."""
+        make_corpus_from_pages(directory, output_filepath, glob_pattern=glob_pattern)
+
+    # Start cli
+    cli()
