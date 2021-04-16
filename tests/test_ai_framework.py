a20c48f9f352376785d4acb54ee6c4a775f83ce0
GalacticGlum <nerftactic09@gmail.com>
2021-04-16 04:28:16 -0400

    Updated test_ai_framework

--- tests/test_ai_framework.py
+++ tests/test_ai_framework.py
@@ -3,14 +3,22 @@ import pprint
 from hsbg.ai import RandomPlayer, MCTSPlayer, run_game, run_games
 
 if __name__ == '__main__':
-    start_time=time.time()
-    winner, move_sequence = run_game([MCTSPlayer(0), RandomPlayer()])
-    print(f'Player {winner + 1} won the game!')
-    print('Took {:.2f} seconds to run a game'.format(time.time() - start_time))
-    pprint.pprint(move_sequence)
+    wins = 0
+    N_GAMES = 100
+    for _ in range(N_GAMES):
+        start_time=time.time()
+        player = MCTSPlayer(0)
+        winner, move_sequence = run_game([player, RandomPlayer()])
+        print(f'Player {winner + 1} won the game!')
+        print('Took {:.2f} seconds to run a game'.format(time.time() - start_time))
+        if winner == 0:
+            wins+=1
+    print('Player 1 won {:.2f}%% of games'.format(wins / N_GAMES))
+        # pprint.pprint(move_sequence)
+
 
     # start_time=time.time()
     # n = 1000
-    # run_games(n, [RandomPlayer(), MCTSPlayer()], n_jobs=1)
+    # run_games(n, [RandomPlayer(), RandomPlayer()], n_jobs=1)
     # elapsed = time.time() - start_time
     # print('Took {:.3f} seconds to run {} games ({:.3f} seconds per game)'.format(elapsed, n, elapsed / n))
